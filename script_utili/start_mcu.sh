9b74ca99398728e8ba74a81bbb060e9608f18853
AntonioIacono <antonio.iac98@outlook.it>
2024-05-22 18:31:19 +0200

    start mcu

--- script_utili/start_mcu.sh
+++ script_utili/start_mcu.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 #Set up the paths(relatives paths)
-ground_notify_msg="./xmpp/ground_notify_message.py"
+ground_notify_msg="./ground_notify_message.py"
 vsftpd_log="/var/log/vsftpd.log"
-xmpp_bot="./xmpp/bot.py"
+xmpp_bot="./bot.py"
 
 #Set up cycle time
 
@@ -11,9 +11,13 @@ xmpp_bot="./xmpp/bot.py"
 #Start
 python3 "$xmpp_bot" &
 
+#clear log
+> "$vsftpd_log"
+
+#Monitor the log file for new entries
 tail -F $vsftpd_log | while read line;do
 	if echo "$line" | grep -q 'ftp';then
 		echo "File uploaded";
-                /bin/python3 xmpp-mcu/ground_notify_message.py
+                python3 $ground_notify_msg
 	fi
 done
