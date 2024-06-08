a9d61f04425fdaa5345b0434769538084201ebd7
AntonioIacono <antonio.iac98@outlook.it>
2024-06-08 12:35:21 +0200

    Update proxy_trdp.py

--- script_utili/trdp/proxy_trdp.py
+++ script_utili/trdp/proxy_trdp.py
@@ -4,20 +4,53 @@ import argparse
 import threading
 from scapy.all import sniff, sendp, Ether, IP, UDP
 
-# Set di indirizzi multicast rilevati
-detected_multicast_addresses = set()
+multicast_addresses = set()
+
+def parse_trdp_packet(data):
+    # Extract fields from TRDP packets
+    sequenceCounter = struct.unpack('>I', data[0:4])[0]
+    protocolVersion, msgType = struct.unpack('>HH', data[4:8])
+    comId = struct.unpack('>I', data[8:12])[0]
+    etbTopoCnt = struct.unpack('>I', data[12:16])[0]
+    opTrnTopoCnt = struct.unpack('>I', data[16:20])[0]
+    datasetLength = struct.unpack('>I', data[20:24])[0]
+    reserved01 = struct.unpack('>I', data[24:28])[0]
+    replyComId = struct.unpack('>I', data[28:32])[0]
+    replyIpAddress = '.'.join(map(str, data[32:36]))
+    headerFcs = struct.unpack('>I', data[36:40])[0]
+
+    life = data[40]
+    check = data[41]
+    dataset = ''.join(f'{byte:08b}' for byte in data[42:])
+
+    return {
+        'sequenceCounter': sequenceCounter,
+        'protocolVersion': protocolVersion,
+        'msgType': msgType,
+        'comId': comId,
+        'etbTopoCnt': etbTopoCnt,
+        'opTrnTopoCnt': opTrnTopoCnt,
+        'datasetLength': datasetLength,
+        'reserved01': reserved01,
+        'replyComId': replyComId,
+        'replyIpAddress': replyIpAddress,
+        'headerFcs': headerFcs,
+        'life': life,
+        'check': check,
+        'dataset': dataset
+    }
 
 def forward_packet(data, forward_interface, src_ip, dst_ip, dst_port):
-    # Costruisce il pacchetto Ethernet
+    # Build the Ethernet packet
     ether = Ether()
     
-    # Costruisce il pacchetto IP
+    # Build the IP packet
     ip = IP(src=src_ip, dst=dst_ip)
     
-    # Costruisce il pacchetto UDP
+    # Build the UDP packet
     udp = UDP(dport=dst_port, sport=dst_port)
     
-    # Invia il pacchetto
+    # Send the packet
     packet = ether / ip / udp / data
     sendp(packet, iface=forward_interface, verbose=0)
 
@@ -26,54 +59,42 @@ def listen_udp_multicast(multicast_ip, port, listen_ip, forward_interface, sourc
     udp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     udp_socket.bind((listen_ip, port))
 
-    # Unisce al gruppo multicast
+    # Join multicast group
     mreq = struct.pack('4s4s', socket.inet_aton(multicast_ip), socket.inet_aton(listen_ip))
     udp_socket.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
 
-    print(f"Ascolto su {multicast_ip}:{port} tramite {listen_ip}")
+    print(f"Listening on {multicast_ip}:{port} via {listen_ip}")
 
     while True:
         data, addr = udp_socket.recvfrom(1024)
-        print(f"Pacchetto ricevuto da {addr} su {multicast_ip}")
+        print(f"Received packet from {addr} on {multicast_ip}")
 
-        # Inoltra direttamente il pacchetto
-        forward_packet(data, forward_interface, source_ip_forward, multicast_ip, port)
-
-def start_listening_thread(multicast_ip, port, listen_ip, forward_interface, source_ip_forward):
-    thread = threading.Thread(target=listen_udp_multicast, args=(multicast_ip, port, listen_ip, forward_interface, source_ip_forward))
-    thread.start()
-    return thread
+        parsed_packet = parse_trdp_packet(data)
+        for key, value in parsed_packet.items():
+            print(f"{key}: {value}")
+        # Check if comId is 4003 before forwarding
+        if parsed_packet['comId'] == 40003:
+            forward_packet(data, forward_interface, source_ip_forward, multicast_ip, port)
 
 def packet_callback(packet):
-    if IP in packet and packet[IP].dst.startswith("239."):
+    if IP in packet and (packet[IP].dst.startswith("239.")):
         multicast_ip = packet[IP].dst
-        if multicast_ip not in detected_multicast_addresses:
-            detected_multicast_addresses.add(multicast_ip)
-            print(f"Nuovo indirizzo multicast rilevato: {multicast_ip}")
+        if multicast_ip not in multicast_addresses:
+            multicast_addresses.add(multicast_ip)
+            print(f"Multicast traffic detected on: {multicast_ip}")
+            threading.Thread(target=listen_udp_multicast, args=(multicast_ip, port, listen_ip, forward_interface, source_ip_forward)).start()
 
-def detect_multicast_addresses(interface, detection_time):
-    print(f"Avvio del rilevamento degli indirizzi multicast sull'interfaccia: {interface} per {detection_time} secondi")
-    
-    # Monitora nuovi indirizzi multicast
-    sniff(iface=interface, prn=packet_callback, filter="ip multicast", store=0, timeout=detection_time)
-
-    print("Rilevamento completato. Indirizzi multicast rilevati:")
-    for multicast_ip in detected_multicast_addresses:
-        print(multicast_ip)
-
-def start_listening_threads(port, listen_ip, forward_interface, source_ip_forward):
-    for multicast_ip in detected_multicast_addresses:
-        start_listening_thread(multicast_ip, port, listen_ip, forward_interface, source_ip_forward)
-
+def monitor_multicast_traffic(interface, port, listen_ip, forward_interface,source_ip_forward):
+    print(f"Starting multicast traffic monitoring on interface: {interface}")
+    sniff(iface=interface, prn=packet_callback, filter="ip multicast", store=0)
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Monitora il traffico multicast su un\'interfaccia di rete e lo inoltra a un\'altra interfaccia fisica')
-    parser.add_argument('-i', '--interface', dest='interface', type=str, default='ens3', help='Interfaccia di rete da monitorare')
-    parser.add_argument('-p', '--port', dest='port', type=int, default=17224, help='Porta su cui ascoltare')
-    parser.add_argument('-l', '--listen', dest='listen_ip', type=str, default='0.0.0.0', help='Indirizzo IP locale su cui ascoltare')
-    parser.add_argument('-fi', '--forward_interface', dest='forward_interface', type=str, default='ens5', help='Interfaccia di rete su cui inoltrare il traffico')
-    parser.add_argument('-fs', '--source_ip_forward', dest='source_ip_forward', type=str, default='172.23.0.13', help='Indirizzo IP da cui inoltrare il traffico')
-    parser.add_argument('-t', '--detection_time', dest='detection_time', type=int, default=30, help='Tempo di rilevamento degli indirizzi multicast in secondi')
+    parser = argparse.ArgumentParser(description='Monitor multicast traffic on a network interface and forward it to another physical interface')
+    parser.add_argument('-i', '--interface', dest='interface', type=str, default='ens3', help='Network interface to monitor')
+    parser.add_argument('-p', '--port', dest='port', type=int, default='17224', help='Port to listen on')
+    parser.add_argument('-l', '--listen', dest='listen_ip', type=str, default='0.0.0.0', help='Local IP address to listen on')
+    parser.add_argument('-fi', '--forward_interface', dest='forward_interface', type=str, default='ens5', help='Network interface to forward traffic to')
+    parser.add_argument('-fs', '--source_ip_forward', dest='source_ip_forward', type=str, default='172.23.0.13', help='IP address to forward traffic from')
 
     args = parser.parse_args()
     interface = args.interface
@@ -81,10 +102,4 @@ if __name__ == '__main__':
     listen_ip = args.listen_ip
     forward_interface = args.forward_interface
     source_ip_forward = args.source_ip_forward
-    detection_time = args.detection_time
-
-    # Fase di rilevamento degli indirizzi multicast
-    detect_multicast_addresses(interface, detection_time)
-
-    # Avvio dei thread di ascolto per gli indirizzi rilevati
-    start_listening_threads(port, listen_ip, forward_interface, source_ip_forward)
+    monitor_multicast_traffic(interface, port, listen_ip, forward_interface, source_ip_forward)
