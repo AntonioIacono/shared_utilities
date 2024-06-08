5c201340d1a47b2520fceeca01f6c2a4638a2069
AntonioIacono <antonio.iac98@outlook.it>
2024-06-08 12:31:16 +0200

    Update proxy_trdp.py

--- script_utili/trdp/proxy_trdp.py
+++ script_utili/trdp/proxy_trdp.py
@@ -4,21 +4,20 @@ import argparse
 import threading
 from scapy.all import sniff, sendp, Ether, IP, UDP
 
-# Set of multicast addresses to filter
-multicast_addresses = {"239.18.1.1", "239.18.1.10", "239.13.1.1", "239.13.1.10"}
+# Set di indirizzi multicast rilevati
 detected_multicast_addresses = set()
 
 def forward_packet(data, forward_interface, src_ip, dst_ip, dst_port):
-    # Build the Ethernet packet
+    # Costruisce il pacchetto Ethernet
     ether = Ether()
     
-    # Build the IP packet
+    # Costruisce il pacchetto IP
     ip = IP(src=src_ip, dst=dst_ip)
     
-    # Build the UDP packet
+    # Costruisce il pacchetto UDP
     udp = UDP(dport=dst_port, sport=dst_port)
     
-    # Send the packet
+    # Invia il pacchetto
     packet = ether / ip / udp / data
     sendp(packet, iface=forward_interface, verbose=0)
 
@@ -27,17 +26,17 @@ def listen_udp_multicast(multicast_ip, port, listen_ip, forward_interface, sourc
     udp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     udp_socket.bind((listen_ip, port))
 
-    # Join multicast group
+    # Unisce al gruppo multicast
     mreq = struct.pack('4s4s', socket.inet_aton(multicast_ip), socket.inet_aton(listen_ip))
     udp_socket.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
 
-    print(f"Listening on {multicast_ip}:{port} via {listen_ip}")
+    print(f"Ascolto su {multicast_ip}:{port} tramite {listen_ip}")
 
     while True:
         data, addr = udp_socket.recvfrom(1024)
-        print(f"Received packet from {addr} on {multicast_ip}")
+        print(f"Pacchetto ricevuto da {addr} su {multicast_ip}")
 
-        # Directly forward the packet
+        # Inoltra direttamente il pacchetto
         forward_packet(data, forward_interface, source_ip_forward, multicast_ip, port)
 
 def start_listening_thread(multicast_ip, port, listen_ip, forward_interface, source_ip_forward):
@@ -50,26 +49,31 @@ def packet_callback(packet):
         multicast_ip = packet[IP].dst
         if multicast_ip not in detected_multicast_addresses:
             detected_multicast_addresses.add(multicast_ip)
-            print(f"New multicast address detected: {multicast_ip}")
-            start_listening_thread(multicast_ip, port, listen_ip, forward_interface, source_ip_forward)
+            print(f"Nuovo indirizzo multicast rilevato: {multicast_ip}")
 
-def monitor_multicast_traffic(interface, port, listen_ip, forward_interface, source_ip_forward):
-    print(f"Starting multicast traffic monitoring on interface: {interface}")
+def detect_multicast_addresses(interface, detection_time):
+    print(f"Avvio del rilevamento degli indirizzi multicast sull'interfaccia: {interface} per {detection_time} secondi")
     
-    # Start threads for predefined multicast addresses
-    for multicast_ip in multicast_addresses:
+    # Monitora nuovi indirizzi multicast
+    sniff(iface=interface, prn=packet_callback, filter="ip multicast", store=0, timeout=detection_time)
+
+    print("Rilevamento completato. Indirizzi multicast rilevati:")
+    for multicast_ip in detected_multicast_addresses:
+        print(multicast_ip)
+
+def start_listening_threads(port, listen_ip, forward_interface, source_ip_forward):
+    for multicast_ip in detected_multicast_addresses:
         start_listening_thread(multicast_ip, port, listen_ip, forward_interface, source_ip_forward)
-    
-    # Monitor for new multicast addresses
-    sniff(iface=interface, prn=packet_callback, filter="ip multicast", store=0)
+
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Monitor multicast traffic on a network interface and forward it to another physical interface')
-    parser.add_argument('-i', '--interface', dest='interface', type=str, default='ens3', help='Network interface to monitor')
-    parser.add_argument('-p', '--port', dest='port', type=int, default=17224, help='Port to listen on')
-    parser.add_argument('-l', '--listen', dest='listen_ip', type=str, default='0.0.0.0', help='Local IP address to listen on')
-    parser.add_argument('-fi', '--forward_interface', dest='forward_interface', type=str, default='ens5', help='Network interface to forward traffic to')
-    parser.add_argument('-fs', '--source_ip_forward', dest='source_ip_forward', type=str, default='172.23.0.13', help='IP address to forward traffic from')
+    parser = argparse.ArgumentParser(description='Monitora il traffico multicast su un\'interfaccia di rete e lo inoltra a un\'altra interfaccia fisica')
+    parser.add_argument('-i', '--interface', dest='interface', type=str, default='ens3', help='Interfaccia di rete da monitorare')
+    parser.add_argument('-p', '--port', dest='port', type=int, default=17224, help='Porta su cui ascoltare')
+    parser.add_argument('-l', '--listen', dest='listen_ip', type=str, default='0.0.0.0', help='Indirizzo IP locale su cui ascoltare')
+    parser.add_argument('-fi', '--forward_interface', dest='forward_interface', type=str, default='ens5', help='Interfaccia di rete su cui inoltrare il traffico')
+    parser.add_argument('-fs', '--source_ip_forward', dest='source_ip_forward', type=str, default='172.23.0.13', help='Indirizzo IP da cui inoltrare il traffico')
+    parser.add_argument('-t', '--detection_time', dest='detection_time', type=int, default=30, help='Tempo di rilevamento degli indirizzi multicast in secondi')
 
     args = parser.parse_args()
     interface = args.interface
@@ -77,5 +81,10 @@ if __name__ == '__main__':
     listen_ip = args.listen_ip
     forward_interface = args.forward_interface
     source_ip_forward = args.source_ip_forward
+    detection_time = args.detection_time
 
-    monitor_multicast_traffic(interface, port, listen_ip, forward_interface, source_ip_forward)
+    # Fase di rilevamento degli indirizzi multicast
+    detect_multicast_addresses(interface, detection_time)
+
+    # Avvio dei thread di ascolto per gli indirizzi rilevati
+    start_listening_threads(port, listen_ip, forward_interface, source_ip_forward)
