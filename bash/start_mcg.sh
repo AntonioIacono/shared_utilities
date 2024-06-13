9b2a7d659e50f77c2d6f265cf6ce5dbaa5f6c559
AntonioIacono <antonio.iac98@outlook.it>
2024-06-13 13:55:38 +0200

    Update start_mcg.sh

--- bash/start_mcg.sh
+++ bash/start_mcg.sh
@@ -1,38 +1,37 @@
 #!/bin/bash
 
-#Set up the paths (relatives)
+# Set up the paths (relatives)
 send_file_ftp="./../ftp/send_file.py"
 file_to_send="./../ftp/test.txt"
 xmpp_bot="./../xmpp/bot.py"
 xmpp_presence="./../xmpp/presence.py"
 
-#Set timer
+# Set timer
 ftp_time=2 # every 2 seconds
-presence_time=3 #every 3 seconds
+presence_time=3 # every 3 seconds
 
+# Define functions
 
-#Define functions
-
-function start_bot{
-    python3 "$xmpp_bot" -j mcg@xmpp-server.sw1.multimedia.arpa -p password -t mcu@xmpp-server.sw1.multimedia.arpa  &
+function start_bot {
+    python3 "$xmpp_bot" -j mcg@xmpp-server.sw1.multimedia.arpa -p password -t mcu@xmpp-server.sw1.multimedia.arpa &
 }
 
-function send_file{
-    echo "FTP Sending";
+function send_file {
+    echo "FTP Sending"
     python3 "$send_file_ftp"
     sleep $ftp_time 
 }
 
-function send_presence_msg{
+function send_presence_msg {
     python3 "$xmpp_presence" -j mcg@xmpp-server.sw1.multimedia.arpa -p password -t mcu@xmpp-server.sw1.multimedia.arpa  
 }
 
-#START
+# START
 
 start_bot &
 
 while true; do
-    send_file_ftp &
+    send_file &
     send_presence_msg &
     wait
 done
