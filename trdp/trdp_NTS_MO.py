57a1b6308d3d3a8196597fdd8c69914084dece2a
AntonioIacono <antonio.iac98@outlook.it>
2024-06-15 11:09:32 +0200

    ok

--- trdp/trdp_NTS_MO.py
+++ trdp/trdp_NTS_MO.py
@@ -4,8 +4,9 @@ import time
 import argparse
 import threading
 import random
+import netifaces
 
-def createMessage(ipAddress,port,timeValue, sequenceCounter, protocolVersion, msgType, comId, etbTopoCnt, opTrnTopoCnt, datasetLength, reserved01, replyComId, replyIpAddress, headerFcs, dataset,lifeenabled, checkenabled, life):
+def createMessage(ipAddress,port,timeValue, sequenceCounter, protocolVersion, msgType, comId, etbTopoCnt, opTrnTopoCnt, datasetLength, reserved01, replyComId, replyIpAddress, headerFcs, dataset,lifeenabled, checkenabled, life, source_ip):
     while True:
         sequenceCounter = sequenceCounter + 1
         life = life + 1 if lifeenabled else 1
@@ -27,7 +28,7 @@ def createMessage(ipAddress,port,timeValue, sequenceCounter, protocolVersion, ms
         for value in ipSplit:
             array.append(int(value))
         values_to_pack = [valore for valore in array]
-        print(array)
+        #print(array)
         value10 = struct.pack('B'* len(array), *array)
             
         value11 = struct.pack('>I',headerFcs)
@@ -43,31 +44,54 @@ def createMessage(ipAddress,port,timeValue, sequenceCounter, protocolVersion, ms
         value7 = struct.pack('>I',len(value15))
 
         payload = value1+mettiInsieme+value4+value5+value6+value7+value8+value9+value10+value11+value15
-        send_udp_packet(ipAddress, port, payload, time)
-        time.sleep(timeValue/1004)
+        send_udp_packet(ipAddress, port, payload, source_ip)
+        time.sleep(timeValue/1000)
 
 
-def send_udp_packet(ip_address, port, payload, time_value):
-
+def send_udp_packet(ip_address, port, payload, source_ip):
     udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    udp_socket.bind((source_ip, 0))
     try:
         # Invio del pacchetto UDP
         udp_socket.sendto(payload, (ip_address, port))
-        print(f"Packet sent to {ip_address}:{port}")
+        #print(f"Packet sent to {ip_address}:{port}")
     except Exception as e:
         print(f"Error: {e}")
     finally:
         # Chiusura del socket
         udp_socket.close()
 
-def start_thread(ipAddress, port, timeValue, sequenceCounter, protocolVersion, msgType, comId, etbTopoCnt, opTrnTopoCnt, datasetLength, reserved01, replyComId, replyIpAddress, headerFcs, dataset, lifeenabled, checkenabled, life):
-    thread = threading.Thread(target=createMessage, args=(ipAddress, port, timeValue, sequenceCounter, protocolVersion, msgType, comId, etbTopoCnt, opTrnTopoCnt, datasetLength, reserved01, replyComId, replyIpAddress, headerFcs, dataset, lifeenabled, checkenabled, life))
+def start_thread(ipAddress, port, timeValue, sequenceCounter, protocolVersion, msgType, comId, etbTopoCnt, opTrnTopoCnt, datasetLength, reserved01, replyComId, replyIpAddress, headerFcs, dataset, lifeenabled, checkenabled, life, source_ip):
+    thread = threading.Thread(target=createMessage, args=(ipAddress, port, timeValue, sequenceCounter, protocolVersion, msgType, comId, etbTopoCnt, opTrnTopoCnt, datasetLength, reserved01, replyComId, replyIpAddress, headerFcs, dataset, lifeenabled, checkenabled, life, source_ip))
     thread.start()
 
 def create_dataset(dataset_length):
     num_bits = dataset_length * 8
     dataset = ''.join(random.choice('01') for _ in range(num_bits))
     return dataset
+
+def check_interface_ip(interface):
+    try:
+        addresses = netifaces.ifaddresses(interface)
+        if netifaces.AF_INET in addresses:
+            ip_info = addresses[netifaces.AF_INET][0]
+            ip_address = ip_info.get('addr', None)
+            if ip_address:
+                return ip_address
+        return None
+    except ValueError:
+        return None
+
+def wait_for_interface_ip(interface, timeout=60, check_interval=1):
+    start_time = time.time()
+    while True:
+        ip_address = check_interface_ip(interface)
+        if ip_address:
+            return ip_address
+        if time.time() - start_time > timeout:
+            return None
+        time.sleep(check_interval)
+
 
 if __name__ == '__main__':
 
@@ -91,30 +115,73 @@ if __name__ == '__main__':
     parser.add_argument('--lifeenabled', action='store_true', required=False, help='Enable life field increment')
     parser.add_argument('--checkenabled', action='store_true', required=False, help='Enable check field')
     parser.add_argument('--life', dest='life', type=int, default=0, required=False, help='Initial value of field life')
-
+    parser.add_argument('--interface', dest='interface', type=str, default=0, required=False, help='Network interface')
     args = parser.parse_args()
 
 
-    ip_multicast = "172.16.1.140"
-    comid = 4003
-    dataset_life = 200
-    port = 17224
+    #Wait for interfaces set up
+    # Esempio di utilizzo
+interfaccia = "ens3"
+source_ip = wait_for_interface_ip(interfaccia, timeout=60, check_interval=1)
+
     
-    """
-    start_thread(ipAddress, port, timeValue, sequenceCounter, protocolVersion, msgType, 
-                 comId, etbTopoCnt, opTrnTopoCnt, datasetLength, reserved01, replyComId, 
-                 replyIpAddress, headerFcs, dataset, lifeenabled, checkenabled, life)
-    """
+"""
+start_thread(ipAddress, port, timeValue, sequenceCounter, protocolVersion, msgType, 
+                comId, etbTopoCnt, opTrnTopoCnt, datasetLength, reserved01, replyComId, 
+                replyIpAddress, headerFcs, dataset, lifeenabled, checkenabled, life)
+"""
 
 
-    #msgType=29264 -> ENA request
-    #msgType=28752 -> ENA replay
-    
-    
-    ##ComID 1301
-    dataset = create_dataset(330)
-    start_thread("172.16.1.20", port, dataset_life, 4035626, 1, 29264,               
-                40003, 0, 0, 4, 0, 0,                                   
-                "0.0.0.0", 3572351821, dataset, True, True, 0)     
-        
-    
+#msgType=29264 -> ENA request
+#msgType=28752 -> ENA replay
+
+
+## Parameters dataset with ComID 1301
+ip_multicast = "239.110.1.1"
+port = 17224
+dataset_life = 200
+sequenceCounter = 4035626
+protocolVersion = 1
+msgType = 28752
+comId = 1301
+etbTopoCnt = 0
+opTrnTopoCnt = 0
+datasetLength = 500
+reserved01 = 4
+replyComId = 0
+replyIpAddress = "0.0.0.0"
+headerFcs = 3572351821
+dataset = create_dataset(datasetLength - 2) # 2 bytes is for the header
+lifeenabled = True
+checkenabled = True
+life = 0
+
+start_thread(ip_multicast, port, dataset_life, sequenceCounter, protocolVersion, msgType, 
+            comId, etbTopoCnt, opTrnTopoCnt, datasetLength, reserved01, replyComId, 
+            replyIpAddress, headerFcs, dataset, lifeenabled, checkenabled, life, source_ip)
+
+## Parameters dataset with ComID 1303
+ip_multicast = "239.110.1.1"
+port = 17224
+dataset_life = 10000
+sequenceCounter = 4035626
+protocolVersion = 1
+msgType = 28752
+comId = 1303
+etbTopoCnt = 0
+opTrnTopoCnt = 0
+datasetLength = 350 
+reserved01 = 4
+replyComId = 0
+replyIpAddress = "0.0.0.0"
+headerFcs = 3572351821
+dataset = create_dataset(datasetLength - 2) # 2 bytes is for the header
+lifeenabled = True
+checkenabled = True
+life = 0
+
+start_thread(ip_multicast, port, dataset_life, sequenceCounter, protocolVersion, msgType, 
+            comId, etbTopoCnt, opTrnTopoCnt, datasetLength, reserved01, replyComId, 
+            replyIpAddress, headerFcs, dataset, lifeenabled, checkenabled, life, source_ip)
+
+
--- trdp/trdp_NTS_ON_A.py
+++ trdp/trdp_NTS_ON_A.py
@@ -161,28 +161,28 @@ start_thread(ip_multicast, port, dataset_life, sequenceCounter, protocolVersion,
             replyIpAddress, headerFcs, dataset, lifeenabled, checkenabled, life, source_ip)
 
 ## Parameters dataset with ComID 20008
-ip_multicast = "239.13.1.1"
-port = 17224
-dataset_life = 50
-sequenceCounter = 4035626
-protocolVersion = 1
-msgType = 28752
-comId = 20008
-etbTopoCnt = 0
-opTrnTopoCnt = 0
-datasetLength = 24 #to be defined
-reserved01 = 4
-replyComId = 0
-replyIpAddress = "0.0.0.0"
-headerFcs = 3572351821
-dataset = create_dataset(datasetLength - 2) # 2 bytes is for the header
-lifeenabled = True
-checkenabled = True
-life = 0
+#ip_multicast = "239.13.1.1"
+#port = 17224
+#dataset_life = 50
+#sequenceCounter = 4035626
+#protocolVersion = 1
+#msgType = 28752
+#comId = 20008
+#etbTopoCnt = 0
+#opTrnTopoCnt = 0
+#datasetLength = 24 #to be defined
+#reserved01 = 4
+#replyComId = 0
+#replyIpAddress = "0.0.0.0"
+#headerFcs = 3572351821
+#dataset = create_dataset(datasetLength - 2) # 2 bytes is for the header
+#lifeenabled = True
+#checkenabled = True
+#life = 0
 
-start_thread(ip_multicast, port, dataset_life, sequenceCounter, protocolVersion, msgType, 
-            comId, etbTopoCnt, opTrnTopoCnt, datasetLength, reserved01, replyComId, 
-            replyIpAddress, headerFcs, dataset, lifeenabled, checkenabled, life, source_ip)
+#start_thread(ip_multicast, port, dataset_life, sequenceCounter, protocolVersion, msgType, 
+#            comId, etbTopoCnt, opTrnTopoCnt, datasetLength, reserved01, replyComId, 
+#            replyIpAddress, headerFcs, dataset, lifeenabled, checkenabled, life, source_ip)
 
 
 ## Parameters dataset with ComID 13010
