046ca8011f4bb4c3f1cca31f8f7cf5c24a577ac2
AntonioIacono <antonio.iac98@outlook.it>
2024-06-16 10:20:48 +0200

    Update trdp_NTS_MO.py

--- trdp/trdp_NTS_MO.py
+++ trdp/trdp_NTS_MO.py
@@ -120,7 +120,7 @@ def createMessage(ipAddress, port, timeValue, sequenceCounter, protocolVersion, 
         header_without_crc = value1 + struct.pack('HH', protocolVersion, msgType) + value4 + value5 + value6 + value7 + value8 + value9 + value10
         print(len(header_without_crc))
         # Calculate the CRC over the header
-        headerFcs = fcs32(header_without_crc, 32, headerFcs)
+        #headerFcs = fcs32(header_without_crc, 32, headerFcs)
         value11 = struct.pack('>I', headerFcs)
 
         # Complete header with CRC and payload
