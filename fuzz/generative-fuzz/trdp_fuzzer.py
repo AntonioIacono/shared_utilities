b15a572044eb886648b04dd8e110c45bb2875211
AntonioIacono <antonio.iac98@outlook.it>
2024-06-25 12:33:16 +0200

    Update trdp_fuzzer.py

--- fuzz/generative-fuzz/trdp_fuzzer.py
+++ fuzz/generative-fuzz/trdp_fuzzer.py
@@ -46,6 +46,7 @@ def createMessage(ipAddress, port, timeValue, sequenceCounter, protocolVersion, 
             if 'replyIpAddress' in fuzz_fields:
                 replyIpAddress = '.'.join(str(random.randint(0, 255)) for _ in range(4))
             if 'dataset' in fuzz_fields:
+                print("qui")
                 datasetLength = random.randint(2**8, 2**32 - 1)
                 dataset = create_dataset(datasetLength - 2)
             if 'life' in fuzz_fields:
