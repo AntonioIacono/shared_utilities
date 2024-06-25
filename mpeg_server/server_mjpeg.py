23bf0789bf42646184c04951b53e3ae1ca8640ec
AntonioIacono <antonio.iac98@outlook.it>
2024-06-25 20:14:15 +0200

    Update server_mjpeg.py

--- mpeg_server/server_mjpeg.py
+++ mpeg_server/server_mjpeg.py
@@ -39,17 +39,17 @@ def generate_stream(video_path):
         '-stream_loop', '-1',  # Loop indefinitely
         '-i', video_path,
         '-f', 'mp4',
-        '-vcodec', 'libx264',
-        '-preset', 'fast',
+        #'-vcodec', 'libx264',
+        #'-preset', 'fast',
         #'-r', '15',  # Frame rate
         #'-s', '1280x720',  # Resolution
         #'-b:v', '1M',  # Bitrate (adjust as needed)
         #'-bufsize', '2M',
         #'-pix_fmt', 'yuv420p',
-        '-movflags', 'frag_keyframe+empty_moov+default_base_moof',
-        '-an',
-        '-sn',
-        'pipe:1'
+        #'-movflags', 'frag_keyframe+empty_moov+default_base_moof',
+        #'-an',
+        #'-sn',
+        #'pipe:1'
     ]
 
 
